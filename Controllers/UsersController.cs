e3fdac8ad89b3905cde72a251f040da01121f855
Nicolai-Jensen <74540161+Nicolai-Jensen@users.noreply.github.com>
2024-11-06 10:36:17 +0100

    Initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using HovedOpgaveWebAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HovedOpgaveWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // Temporary in-memory list to store users
+        private static List<User> users = new List<User>
+        {
+            new User { Id = 1, Name = "Jane", Age = 30, Gender = "Female" },
+            new User { Id = 2, Name = "John", Age = 25, Gender = "Male" }
+        };
+
+        // GET: api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return Ok(users);
+        }
+
+        // GET: api/users/{id}
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        // POST: api/users
+        [HttpPost]
+        public ActionResult<User> CreateUser(User newUser)
+        {
+            newUser.Id = users.Count > 0 ? users.Max(u => u.Id) + 1 : 1;
+            users.Add(newUser);
+            return CreatedAtAction(nameof(GetUser), new { id = newUser.Id }, newUser);
+        }
+
+        // PUT: api/users/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateUser(int id, User updatedUser)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Name = updatedUser.Name;
+            user.Age = updatedUser.Age;
+            user.Gender = updatedUser.Gender;
+
+            return NoContent();
+        }
+
+        // DELETE: api/users/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteUser(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            users.Remove(user);
+            return NoContent();
+        }
+    }
+}
