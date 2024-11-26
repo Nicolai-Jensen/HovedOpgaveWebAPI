e8293cf205fde839fbda1c530275c458b7e5772d
Nicolai-Jensen <74540161+Nicolai-Jensen@users.noreply.github.com>
2024-11-26 11:40:30 +0100

    Updated API
    Now uses CompositeKeys with 2 strings:
    UserId and GameID
    
    Controller now requires both keys when making requests

---
+++ Migrations/20241126101201_AddCompositeKey.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HovedOpgaveWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompositeKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GameData",
+                table: "GameData");
+
+            migrationBuilder.Sql("ALTER TABLE \"GameData\" ALTER COLUMN \"UserId\" DROP IDENTITY IF EXISTS;");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GameId",
+                table: "GameData",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "GameData",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GameData",
+                table: "GameData",
+                columns: new[] { "UserId", "GameId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GameData",
+                table: "GameData");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GameId",
+                table: "GameData",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "GameData",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GameData",
+                table: "GameData",
+                column: "UserId");
+        }
+    }
+}
